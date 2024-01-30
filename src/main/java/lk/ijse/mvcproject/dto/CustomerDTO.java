4b7a12ab296b329ba08627ed5e9f72a96687111a
kash2005 <kashmifernando3@gmail.com>
2024-01-30 20:20:10 +0530

    add CustomerDTO

--- src/main/java/lk/ijse/mvcproject/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/mvcproject/dto/CustomerDTO.java
@@ -1,4 +1,16 @@
 package lk.ijse.mvcproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CustomerDTO {
+    private String customerId;
+    private String name;
+    private String address;
+    private String email;
+    private String contact;
 }
