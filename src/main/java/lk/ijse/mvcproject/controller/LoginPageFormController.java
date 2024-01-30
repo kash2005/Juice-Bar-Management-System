2d0ae5a16fcc2a1beecbeeab1a87adf371850690
kash2005 <kashmifernando3@gmail.com>
2024-01-30 20:24:00 +0530

    complete loginForm

--- src/main/java/lk/ijse/mvcproject/controller/LoginPageFormController.java
+++ src/main/java/lk/ijse/mvcproject/controller/LoginPageFormController.java
@@ -5,10 +5,13 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -16,9 +19,11 @@ import lk.ijse.mvcproject.dto.UserDTO;
 import lk.ijse.mvcproject.model.UserModel;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class LoginPageFormController {
+public class LoginPageFormController implements Initializable {
     @FXML
     private TextField userNameId;
 
@@ -29,7 +34,34 @@ public class LoginPageFormController {
     private PasswordField passwordId;
 
     @FXML
+    private TextField textPasswordId;
+
+    @FXML
     private AnchorPane root;
+
+    @FXML
+    private ImageView showImg;
+
+    @FXML
+    private ImageView hideImg;
+
+    @FXML
+    void hidePasswordOnMouseClicked(MouseEvent event) {
+        passwordId.setText(textPasswordId.getText());
+        hideImg.setVisible(true);
+        passwordId.setVisible(true);
+        showImg.setVisible(false);
+        textPasswordId.setVisible(false);
+    }
+
+    @FXML
+    void showPasswordOnMouseClicked(MouseEvent event) {
+        textPasswordId.setText(passwordId.getText());
+        showImg.setVisible(true);
+        textPasswordId.setVisible(true);
+        hideImg.setVisible(false);
+        passwordId.setVisible(false);
+    }
 
     @FXML
     void loginBtnOnAction(ActionEvent event) throws IOException {
@@ -50,5 +82,23 @@ public class LoginPageFormController {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+        loginBtn.fire();
+    }
+
+    @FXML
+    void txtUserNameOnAction(ActionEvent event) {
+        passwordId.requestFocus();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        userNameId.requestFocus();
+        textPasswordId.setVisible(false);
+        hideImg.setVisible(true);
+        passwordId.setVisible(true);
     }
 }
